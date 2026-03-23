b5c83f223f67d054354d05b19fd9a7f935ee89a3
alous12 <alejandro.machaca@ucb.edu.bo>
2026-03-23 16:09:48 -0400

    Final

--- js/services.js
+++ js/services.js
@@ -4,34 +4,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
     botonesFiltro.forEach(boton => {
         boton.addEventListener('click', () => {
-            // 1. Manejar la clase activa en los botones
             botonesFiltro.forEach(btn => btn.classList.remove('active'));
             boton.classList.add('active');
 
-            // 2. Obtener la categoría a filtrar
             const categoria = boton.getAttribute('data-filter');
-
-            // 3. Filtrar las habitaciones con una pequeña animación
             habitaciones.forEach(habitacion => {
                 const categoriaHabitacion = habitacion.getAttribute('data-category');
 
                 if (categoria === 'todas' || categoria === categoriaHabitacion) {
-                    // Mostrar
                     habitacion.style.display = 'flex';
-                    // Pequeño timeout para que la animación de entrada se note
                     setTimeout(() => {
                         habitacion.style.opacity = '1';
                         habitacion.style.transform = 'scale(1)';
                     }, 10);
                 } else {
-                    // Ocultar
                     habitacion.style.opacity = '0';
                     habitacion.style.transform = 'scale(0.95)';
                     setTimeout(() => {
                         habitacion.style.display = 'none';
-                    }, 300); // Tiempo coincide con la transición de CSS
+                    }, 300); 
                 }
             });
+        });
+    });
+
+    //Boton de Scroll
+    const btnScrollTop = document.getElementById('btn-scroll-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            btnScrollTop.classList.add('show');
+        } else {
+            btnScrollTop.classList.remove('show');
+        }
+    });
+    btnScrollTop.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' 
         });
     });
 });
